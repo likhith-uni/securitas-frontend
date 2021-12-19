2752db302aab1d5edf5292dc177a5d44a87cbf95
Tanmay Chaturvedi <tanmay.chaturvedi18@st.niituniversity.in>
2021-12-19 18:04:08 +0530

    fixed background and scanner

--- scan.py
+++ scan.py
@@ -12,3 +12,6 @@ def placeholder_scan_logic():
 def post_vulns(project_id):
     for vuln in placeholder_scan_logic():
         print(requests.post('http://localhost:8000/'+'user/'+str(project_id)+"/"+"vulnerabilities",json=vuln).text)
+
+def max_vuln_check():
+    print('Check 0/1')
